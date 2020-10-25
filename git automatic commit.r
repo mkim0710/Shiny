458ae68c968720a43253000ad1d2d54b505744be
mkim0710 <mkim0710@gmail.com>
2020-10-25 10:34:07 +0000

    1

---
+++ git automatic commit.r
@@ -0,0 +1,19 @@
+# git automatic commit.r
+# # To paste text in the terminal use Shift + Insert.
+
+system("git status")
+system("git pull")
+system("git add .")
+system('git commit -m "automatic commit using system()"')
+system("git push")
+# system("git status")
+
+# system("git fetch")
+
+
+
+
+
+
+
+# Caution) do not forget to add .gitignore to .gitignore
